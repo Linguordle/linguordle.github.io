0e49e5e66d7c3167856a8a5d8c5e32d2437b02bf
Linguordle <81645378+Linguordle@users.noreply.github.com>
2025-07-21 13:29:35 -0400

    Update game.js

--- web/game.js
+++ web/game.js
@@ -1,7 +1,7 @@
 window.addEventListener('DOMContentLoaded', () => {
 
 const useEasyMode = localStorage.getItem('easyMode') === 'true';
-const LANGUAGE_DATA = useEasyMode ? LANGUAGE_DATA_EASY : LANGUAGE_DATA;
+const LANGUAGE_DATA = useEasyMode ? LANGUAGE_DATA_EASY : LANGUAGE_DATA_FULL;
 const languageList = Object.keys(LANGUAGE_DATA);
 const MAX_GUESSES = 15;
 let guessesLeft = MAX_GUESSES;
