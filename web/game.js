97803c72cf7cb482930ecac97ced515cf709b443
Linguordle <81645378+Linguordle@users.noreply.github.com>
2025-07-30 11:44:51 -0400

    Second attempt to fix unrelated

--- web/game.js
+++ web/game.js
@@ -511,9 +511,9 @@ function renderTree(data, unrelatedList = []) {
     const verticalSpacing = 28;
     const unrelatedData = unrelatedList.map((name, i) => {
         if (!unrelatedNodePositions[name]) {
-            const baseX = innerWidth * 0.5;
+            const baseX = innerWidth * 1.5;
             const spacingY = 26; // vertical spacing between nodes
-            const jitterX = 20;   // small horizontal jitter to keep the "floating" feel
+            const jitterX = 40;   // small horizontal jitter to keep the "floating" feel
 
             unrelatedNodePositions[name] = {
                 x: baseX + (Math.random() - 0.5) * jitterX,
